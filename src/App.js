71fa140c8356cac435f8efd6d8c7bc1149678581
Jason Cain <118203097+cainjason@users.noreply.github.com>
2024-05-30 22:00:02 -0400

    bootstrapped tailwindcss

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="app">
+      <Footer />
+      <h2 className="bg-red-200"> Ready to Build </h2>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
